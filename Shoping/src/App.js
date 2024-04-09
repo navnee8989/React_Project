3a582b34a25e35eb0151f5ac10316827cdd2ad2a
navnee8989 <sondarvanavneet444@gmail.com>
2024-04-09 15:48:22 +0530

    Created a Simple Product Card and Add to Card Using Usecontext and props

---
+++ Shoping/src/App.js
@@ -0,0 +1,30 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/navbar";
+import { Shop } from "./pages/shop/shop";
+import { Contact } from "./pages/contact";
+import { Cart } from "./pages/cart/cart";
+import { ProductDetails } from "./pages/productDetails/productDetails";
+import { ShopContextProvider } from "./context/shop-context";
+
+function App() {
+  return (
+    <div className="App">
+      <ShopContextProvider>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Shop />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/product/:id" element={<ProductDetails />} />
+          </Routes>
+         
+        </Router>
+      </ShopContextProvider>
+    </div>
+  );
+}
+
+export default App;
+
